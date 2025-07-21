770df687149ad0d2ee71e8cbca45919a0b98fc03
Ted Tran <TedTran2019@gmail.com>
2025-07-20 17:55:08 -0700

    Coded Student Reminders again

--- ruby/misc/random_problems/student_reminders.rb
+++ ruby/misc/random_problems/student_reminders.rb
@@ -52,3 +52,44 @@ puts(must_register([99]) == ['0-98'])
 puts(must_register([48, 50]) == %w[0-47 49 51-99])
 puts(must_register([47, 50]) == %w[0-46 48-49 51-99])
 puts(must_register([47, 48, 49, 50]) == %w[0-46 51-99])
+
+# Problem revisit on 7/20/25
+# 100 students (IDs 0-99)
+# Sorted list of student IDs
+# - Single missing student (number as str)
+# - If 1+ student missing, range string
+#
+# So basically given a list of students who are present...
+#
+# Find starting number (let's say 0)
+# If we start at 2, we know 0-1 are missing
+# Aka the starting number up to current - 1
+# Exception is the end, last number found (if not 99) + 1 up til' 99
+#
+# starting_number = 0
+# current_number = 48
+# fill in 0-47 then move on
+# starting_number = 49
+# current_number = 50
+# fill in 49 - 49 aka just 49
+# end of array
+# starting_number = 51
+# fill in 51 - 99
+
+def must_register(data)
+  starting_number = 0
+  missing_student_ids = []
+  data.each do |current_number|
+    if current_number == starting_number
+      starting_number += 1
+    else
+      goal = current_number - 1
+      missing_student_ids << (goal == starting_number ? goal.to_s : "#{starting_number}-#{goal}")
+      starting_number = current_number + 1
+    end
+  end
+  if starting_number <= 99
+    missing_student_ids << (starting_number == 99 ? '99' : "#{starting_number}-99")
+  end
+  missing_student_ids
+end
