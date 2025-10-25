dafd557f1fb685f92e1af2a40e81a3bee6f23d57
Ted Tran <TedTran2019@gmail.com>
2025-10-24 20:40:20 -0700

    feat(spiral_matrix): implement spiral matrix
    Implemented spiral matrix, only adding until spiral's length is the flattened matrix's length (don't
    need to deal with odd edge cases). Fun simulation type of problem!

---
+++ ruby/formation_engineering_method/0054_spiral_matrix.md
@@ -0,0 +1,9 @@
+```ruby
+=begin
+Given mxn matrix, return all elements of matrix in spiral order
+left -> right (top +1)
+up -> down (right -1)
+right -> left (bottom -1)
+bottom -> top (left +1)
+=end
+```
---
+++ ruby/lib/0054_spiral_matrix.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+def spiral_order(matrix)
+  spiral = []
+  goal_length = matrix.flatten.length
+  top = 0
+  left = 0
+  right = matrix[0].length - 1
+  bottom = matrix.length - 1
+  until spiral.length == goal_length
+    (left..right).each do |x|
+      break if spiral.length == goal_length
+
+      spiral << matrix[top][x]
+    end
+    top += 1
+
+    (top..bottom).each do |y|
+      break if spiral.length == goal_length
+
+      spiral << matrix[y][right]
+    end
+    right -= 1
+
+    right.downto(left).each do |x|
+      break if spiral.length == goal_length
+
+      spiral << matrix[bottom][x]
+    end
+    bottom -= 1
+
+    bottom.downto(top).each do |y|
+      break if spiral.length == goal_length
+
+      spiral << matrix[y][left]
+    end
+    left += 1
+  end
+  spiral
+end
