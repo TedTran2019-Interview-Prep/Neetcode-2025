f909cf8753b6a6f582a7f928f66192fb4662bda9
Ted Tran <TedTran2019@gmail.com>
2025-12-08 15:20:21 -0800

    feat(0075): sort colors
    Redo, do it three ways: counting, two pass, one pass with three pointer partition (Think about what
    happens in every case, focus on middle)

--- ruby/lib/0075_sort_colors.rb
+++ ruby/lib/0075_sort_colors.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
-def sort_colors(nums)
-  count = Hash.new(0)
-  nums.each { |num| count[num] += 1 }
-  index = 0
-  (0..2).each do |num|
-    count[num].times do
-      nums[index] = num
-      index += 1
-    end
-  end
-end
-
-# One pass three pointer partitioning
+# One pass three pointer partitioning, no extra space
 def sort_colors(nums)
   low = 0
   middle = 0
@@ -29,4 +17,39 @@ def sort_colors(nums)
       high -= 1
     end
   end
+end
+
+# O(N) extra space using hashmap
+def sort_colors(nums)
+  count = Hash.new(0)
+  nums.each { |num| count[num] += 1 }
+  index = 0
+  (0..2).each do |num|
+    count[num].times do
+      nums[index] = num
+      index += 1
+    end
+  end
+end
+
+# Two pass no extra space
+def sort_colors(nums)
+  new_start_idx = put_at_start(0, 0, nums)
+  put_at_start(new_start_idx, 1, nums)
+end
+
+def put_at_start(start_idx, num, nums)
+  last_idx = nums.length - 1
+  while start_idx < last_idx
+    if nums[start_idx] == num
+      start_idx += 1
+    elsif nums[last_idx] == num
+      nums[start_idx], nums[last_idx] = nums[last_idx], nums[start_idx]
+      start_idx += 1
+      last_idx -= 1
+    else
+      last_idx -= 1
+    end
+  end
+  nums[start_idx] == num ? start_idx + 1 : start_idx
 end
